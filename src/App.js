6b4844342dc4e5ee1c8513afe7370113ae70bbc0
hanokoyosei <perizatonolbekova17@gmail.com>
2021-12-03 08:09:27 +0600

    commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import AddTodo from './components/AddTodo/AddTodo';
+import TodoList from './components/TodoList/TodoList';
+
+function App() {  
+  let [todos, setTodos] = useState([]);
+
+  function handleTask(newObj){
+    let newTodos = [...todos];
+
+    newTodos.push(newObj)
+    setTodos(newTodos)
+  }
+
+  function changeStatus(id){
+    const newTodos = todos.map(item => {
+      if(item.id === id){
+        item.status = !item.status
+      }
+      return item
+    })
+
+    setTodos(newTodos)
+  }
+
+  return (
+    <div className="app">
+      <AddTodo 
+        handleTask={handleTask}
+      />
+      <TodoList 
+        todos={todos} 
+        changeStatus={changeStatus}
+      />
+    </div>
+  )
+}
+
+export default App;
